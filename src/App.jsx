c6acad263e3c6d3f5691494c2f4ab1574233acf1
Cristian <calarcon@fi.uba.ar>
2025-12-17 00:48:27 -0300

    estructura basica del portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        ¡Proyecto 'portfolio-dev' listo!
+      </h1>
+    </div>
+  )
+}
+
+export default App
