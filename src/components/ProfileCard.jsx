650b933ce548861e745e1de3c6108be8bdca0036
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-18 17:39:15 -0300

    mt auto para ajustar los links al contenido de la card inferiormente

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -6,14 +6,15 @@ export default function ProfileCard() {
   const { translate } = useLanguage()
 
   return (
-    <div className="bg-card rounded-xl p-6 border border-border/50 ">
+    <div className="bg-card rounded-xl p-6 border border-border/50 flex flex-col h-full">
       {/* Profile Header */}
       <section className="flex justify-items-start gap-6 items-center mb-4">
         <LuUser  size={32} color="default" />
         <h3 className="text-xl font-bold">{translate("profileTitle")}</h3>
       </section>
       <p className="text-foreground/70 mb-6">{translate("profileDesc")}</p>
-      <div className="flex gap-3">
+      {/* Social Links */}
+      <section className="flex gap-3 mt-auto">
         <button className="p-3 bg-secondary hover:bg-secondary/80 rounded-lg transition-colors">
           <LuMail size={20} color="default" />
         </button>
@@ -23,7 +24,7 @@ export default function ProfileCard() {
         <button className="p-3 bg-secondary hover:bg-secondary/80 rounded-lg transition-colors">
           <LuGithub size={20} color="default" />
         </button>
-      </div>
+      </section>
     </div>
   )
 }
