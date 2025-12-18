9676f8d1443cad7c11fbd4c5b3ce0310a1140dd9
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-18 16:46:34 -0300

    se agregan sections para educacion en columna con un gap, luego llevar la fuente a un archivo general

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -11,9 +11,38 @@ export default function EducationCard() {
         <h3 className="text-xl font-bold">{translate("educationTitle")}</h3>
       </section>
       {/* Education Details */}
-      <section className="space-y-2">
-        <p className="text-foreground/70 text-sm">Universidad de Buenos Aires</p>
-        <p className="text-primary text-sm font-medium">2025</p>
+      <section className="flex flex-col gap-6">
+        
+        {/* Educación Formal */}
+        <div className="border-l-2 border-primary pl-4">
+          <h4 className="font-bold text-foreground/70">
+            Ingeniería en Informática
+          </h4>
+          <p className="text-foreground/70">Universidad de Buenos Aires</p>
+          <p className="text-foreground/70 font-semibold">Actualmente 3er año</p>
+        </div>
+
+        {/* Capacitación */}
+        <div className="border-l-2 border-border pl-4">
+          <h4 className="font-bold text-foreground/70  ">
+            Capacitación Full Stack MERN
+          </h4>
+          <p className="text-foreground">Educación IT •</p>
+          <p className="text-foreground/70 font-semibold ">Finalizado en diciembre 2025</p>
+        </div>
+
+        {/* Futuro cercano */}
+        
+        <div className="border-l-2 border-dashed border-border pl-4 italic">
+          <h4 className="text-foreground/70 font-semibold ">
+            Java Backend & Spring Boot
+          </h4>
+          <p className="text-foreground/70 font-semibold ">
+            Próximamente
+          </p>
+        </div>
+        
+
       </section>
     </div>
   )
