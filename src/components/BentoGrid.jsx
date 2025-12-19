7d6afd7f28806064510716898614b2df87ed12fd
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-19 15:12:26 -0300

    cambio de alineacion, proyects con un grid y lo demas con otro

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -11,7 +11,7 @@ export default function BentoGrid() {
     <section id="projects" className="py-20 px-4 bg-background">
       <div className="max-w-6xl mx-auto">
         <h2 className="text-4xl font-bold mb-12 text-center">{translate("featuredWork")}</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-max">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
           <ProjectCard
             title="Codigo Empleo"
             description={translate("codigoEmpleoDescription")}
@@ -28,6 +28,8 @@ export default function BentoGrid() {
             githubLink="https://github.com/CristianAlarconDev/BattleCityTP"
             demoLink=""
           />
+        </div>
+        <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <TechStackCard />
           <ProfileCard  />
           <EducationCard />
