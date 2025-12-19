cfa38e80d5f8c84a7bb5791d19322be3cbdf6004
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-19 16:07:22 -0300

    cambio bck color de navbar, tiene menos opacidad al scrollear

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
 
     return (
         <nav className={`fixed top-0 w-full z-50 transition-all duration-300 ${
-            isScrolled ? "bg-background/80 backdrop-blur-md border-b border-border" : "bg-white/30 px-4 py-2 "
+            isScrolled ? "bg-gray-200/70 backdrop-blur-md border-b border-border" : "bg-gray-200 px-4 py-2 border-b "
         }`}
         >
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
