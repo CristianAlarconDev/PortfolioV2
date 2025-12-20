a422822783b3c76e95883a6186d1f0458e2a7863
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-19 22:45:57 -0300

    uso de text del index css global mas cambio de border

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import { useLanguage } from "../contexts/LanguageContext";
 export default function ProjectCard({ title, description, tags, image, githubLink, demoLink }) {
   const {translate} = useLanguage();
   return (
-    <div className="bg-card rounded-2xl p-6 border  flex flex-col h-full
+    <div className="bg-card rounded-2xl p-6 border border-border flex flex-col h-full
                   transition-all duration-200
                   hover:-translate-y-1 hover:shadow-xl " >
       {/*Project Image */}
@@ -13,14 +13,14 @@ export default function ProjectCard({ title, description, tags, image, githubLin
       </section>
       {/* Project Title & Description */}
       <section>
-        <h3 className="text-xl font-bold mb-2">{title}</h3>
-        <p className="text-foreground/70 mb-4">{description}</p>
+        <h3 className="text-xl font-bold mb-2 text-foretext">{title}</h3>
+        <p className="text-card-text mb-4">{description}</p>
       </section>
       {/* Tags */}
       <section className="flex flex-wrap gap-2 mb-5 mt-auto">
         {tags.map((tag) => (
           <span key={tag} className="px-3 py-1 rounded-full text-xs font-medium
-                      text-foreground/80 border ">
+                      text-foretext border border-border/110">
             {tag}
           </span>
         ))}
@@ -29,11 +29,11 @@ export default function ProjectCard({ title, description, tags, image, githubLin
       {/* Links */}
       <section className="flex gap-4">
         <a href={demoLink} target="_blank" rel="noopener noreferrer" 
-          className="flex items-center gap-2 text-primary hover:text-primary/80 transition-colors font-medium text-sm">
+          className="flex items-center gap-2 text-foretext  transition-colors font-medium text-sm">
           <ExternalLink size={22} /> {translate("proyectCardPreview")}
         </a>
         <a href={githubLink} target="_blank" rel="noopener noreferrer" 
-          className="flex items-center gap-2 text-primary hover:text-primary/80 transition-colors font-medium text-sm">
+          className="flex items-center gap-2 text-foretext text-primary transition-colors font-medium text-sm">
           <LuGithub size={22} /> {translate("proyectCardCode")}
         </a>
       </section>
