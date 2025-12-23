012b42fe1836461df1b4cf6bd1f7849990b7ee93
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-22 22:37:42 -0300

    se suma un border, mismo del color del back, luego llevar a css global y refactorizar los links para que los lea de un array

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
             />
             </div>   
         {/* Gradiente claro semitransparente */}
-        <div className="absolute inset-0 z-10 bg-linear-to-b via-white/52"></div>
+        <div className="absolute inset-0 z-10 bg-linear-to-b via-white/40"></div>
         <div className="relative z-20 max-w-4xl mx-auto text-center">
             <h1 className="text-3xl md:text-5xl font-bold mb-6 text-balance">{translate("title")}</h1>
             <p className="text-xl md:text-2xl text-foreground/70 mb-12 text-balance">{translate("subtitle")}</p>
@@ -21,14 +21,14 @@ export default function Hero() {
                 <div className="flex flex-col sm:flex-row gap-3 justify-center mt-10 w-full sm:w-auto">
 
                 <Link to="projects" smooth={true} duration={500}
-                    className="cursor-pointer bg-black/60 text-white px-8 py-3 rounded-xl font-semibold
-                    flex items-center justify-center gap-2 hover:bg-black/70
+                    className="cursor-pointer bg-black/60 border-b border-black/60 text-white px-8 py-3 rounded-xl font-semibold
+                    flex items-center justify-center gap-2 hover:bg-black/80
                     w-full max-w-sm mx-auto sm:w-auto sm:max-w-none sm:mx-0 whitespace-nowrap ">
                     {translate("viewWork")} <ArrowRight size={18} />
                 </Link>
                 <a href="/CV-Full-Stack.pdf" download="CV_Cristian_Alarcon.pdf"
-                    className="bg-white/30 border  text-black px-8 py-3 rounded-xl font-semibold 
-                    flex items-center justify-center gap-2 hover:bg-white/50
+                    className="bg-white/40 border-white/30 text-black px-8 py-3 rounded-xl font-semibold 
+                    flex items-center justify-center gap-2 hover:bg-white/60
                     w-full max-w-sm mx-auto sm:w-auto sm:max-w-none sm:mx-0 whitespace-nowrap "
                 >
                     {translate("resume")} <Download size={18} />
