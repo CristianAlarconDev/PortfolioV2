f4d71fd7661df546ba8e7d62ab611f8ebffe8871
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-22 22:35:03 -0300

    se suma sombra y se usan variables de env

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -6,7 +6,7 @@ export default function ProfileCard() {
   const { translate } = useLanguage()
 
   return (
-    <section id="contact" className="bg-card rounded-xl p-6 border border-border/90 flex flex-col h-full">
+    <section id="contact" className="bg-card rounded-xl p-6 border border-border/90 flex flex-col h-full shadow-xl">
       {/* Profile Header */}
       <section className="flex justify-items-start gap-6 items-center mb-4">
         <LuUser  size={32} color="default" />
@@ -15,7 +15,7 @@ export default function ProfileCard() {
       <p className="text-card-text mb-6">{translate("profileDesc")}</p>
       {/* Social Links */}
       <section className="flex gap-2 mt-auto">
-        <a href="mailto:calarcon@fi.uba.ar" target="_blank" rel="noopener noreferrer" 
+        <a href={import.meta.env.VITE_EMAIL_URL || ""} target="_blank" rel="noopener noreferrer" 
         className="p-3 transition-colors flex items-center justify-center text-card-text"
         aria-label="Email"
         >
@@ -25,7 +25,7 @@ export default function ProfileCard() {
           </div>
         </a>
 
-        <a href="https://www.linkedin.com/in/cristianalarcondevfiuba" target="_blank" rel="noopener noreferrer"
+        <a href={import.meta.env.VITE_LINKEDIN_URL || ""} target="_blank" rel="noopener noreferrer"
           className="p-3 rounded-lg transition-colors flex items-center justify-center text-card-text"
           aria-label="LinkedIn"
         >
@@ -34,7 +34,7 @@ export default function ProfileCard() {
           <span className="text-card-text">Linkedin</span>
           </div>
         </a>
-        <a href="https://github.com/CristianAlarconDev" target="_blank" rel="noopener noreferrer"
+        <a href={import.meta.env.VITE_GITHUB_URL || ""} target="_blank" rel="noopener noreferrer"
           className="p-3 rounded-lg transition-colors flex items-center justify-center text-card-text"
           aria-label="GitHub"
         >
